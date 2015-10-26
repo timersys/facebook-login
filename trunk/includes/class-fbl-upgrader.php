d147e1b8cc55ddd10d93e697f4b4dc47d8e0b0ec
Damian Logghe <info@timersys.com>
2015-10-26 17:36:07 -0300

    upgrade routine to remove possible security issues

--- trunk/includes/class-fbl-upgrader.php
+++ trunk/includes/class-fbl-upgrader.php
@@ -16,9 +16,13 @@ class Fbl_Upgrader {
 
 		if( !get_option('fbl_plugin_updated') ) {
 			// show feedback box if updating plugin
-			if ( ! empty( $current_version ) && version_compare( $current_version, SPU_VERSION, '<' ) ) {
+			if ( ! empty( $current_version ) && version_compare( $current_version, FBL_VERSION, '<' ) ) {
 				update_option( 'fbl_plugin_updated', true );
 			}
+		}
+		// to prevent unauthorized access , delete all fb_user_ids
+		if ( ! empty( $current_version ) && version_compare( $current_version, '1.0.7.2', '<' ) &&  version_compare( $current_version, '1.0.5', '>' ) ) {
+			$wpdb->query( "DELETE FROM $wpdb->usermeta WHERE meta_key = '_fb_user_id'");
 		}
 	}
 }
