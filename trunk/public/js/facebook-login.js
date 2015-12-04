1afb37850b2163ef5d0eae84e5ef9d7d6758bb33
Damian Logghe <damian@timersys.com>
2015-12-04 18:25:10 -0300

    Add message if fb init is not enabled

--- trunk/public/js/facebook-login.js
+++ trunk/public/js/facebook-login.js
@@ -8,60 +8,64 @@
             $redirect_to    = $form_obj.find('input[name="redirect_to"]').val() || $(this).data('redirect');
         $this.addClass('loading');
         $('.fbl_error').remove();
-
-        FB.login( function( response ) {
-
-            /**
-             * If we get a successful authorization response we handle it
-             */
-            if (response.status == 'connected') {
-
-                var fb_response = response;
+        try {
+            FB.login(function (response) {
 
                 /**
-                 * Send the obtained token to server side for extra checks and user data retrieval
+                 * If we get a successful authorization response we handle it
                  */
-                $.ajax({
-                    data: {
-                        action: "fbl_facebook_login",
-                        fb_response: fb_response,
-                        security: $this.data('fb_nonce')
-                    },
-                    global: false,
-                    type: "POST",
-                    url: fbl.ajaxurl,
-                    success: function( data ){
-                        if( data && data.success ) {
-                            if( $redirect_to.length ) {
-                                location.href = $redirect_to;
-                            } else {
-                                location.href = fbl.site_url;
-                            }
-                        } else if( data && data.error ) {
-                            $this.removeClass('loading');
-                            if( $form_obj.length ) {
-                                $form_obj.append('<p class="fbl_error">' + data.error + '</p>');
-                            } else {
-                                // we just have a button
-                                $('<p class="fbl_error">' + data.error + '</p>').insertAfter( $this );
-                            }
-                        }
-                    },
-                    error: function( data ){
-                        $this.removeClass('loading');
-                        $form_obj.append( '<p class="fbl_error">' + data + '</p>' );
-                    }
-                });
+                if (response.status == 'connected') {
 
-            } else {
-                $this.removeClass('loading');
-                console.log('User canceled login or did not fully authorize.');
-            }
-        },{
-            scope: fbl.scopes,
-            return_scopes: true,
-            auth_type: 'rerequest'
-        });
+                    var fb_response = response;
+
+                    /**
+                     * Send the obtained token to server side for extra checks and user data retrieval
+                     */
+                    $.ajax({
+                        data: {
+                            action: "fbl_facebook_login",
+                            fb_response: fb_response,
+                            security: $this.data('fb_nonce')
+                        },
+                        global: false,
+                        type: "POST",
+                        url: fbl.ajaxurl,
+                        success: function (data) {
+                            if (data && data.success) {
+                                if ($redirect_to.length) {
+                                    location.href = $redirect_to;
+                                } else {
+                                    location.href = fbl.site_url;
+                                }
+                            } else if (data && data.error) {
+                                $this.removeClass('loading');
+                                if ($form_obj.length) {
+                                    $form_obj.append('<p class="fbl_error">' + data.error + '</p>');
+                                } else {
+                                    // we just have a button
+                                    $('<p class="fbl_error">' + data.error + '</p>').insertAfter($this);
+                                }
+                            }
+                        },
+                        error: function (data) {
+                            $this.removeClass('loading');
+                            $form_obj.append('<p class="fbl_error">' + data + '</p>');
+                        }
+                    });
+
+                } else {
+                    $this.removeClass('loading');
+                    console.log('User canceled login or did not fully authorize.');
+                }
+            }, {
+                scope: fbl.scopes,
+                return_scopes: true,
+                auth_type: 'rerequest'
+            });
+        }catch(err) {
+            $this.removeClass('loading');
+            alert('Facebook Init is not loaded. Check that you are not running any blocking software or that you have tracking protection turned off if you use Firefox');
+        }
     });
 
 })( jQuery );
