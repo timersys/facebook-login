521228f2319719cbb12fa7ac661994071a176a5b
Damian Logghe <damian@timersys.com>
2016-02-22 14:15:13 -0300

    hide img by default

--- trunk/public/class-facebook-login-public.php
+++ trunk/public/class-facebook-login-public.php
@@ -95,7 +95,7 @@ class Facebook_Login_Public {
 		if ( isset( $GLOBALS['pagenow'] ) && in_array( $GLOBALS['pagenow'], array( 'wp-login.php', 'wp-register.php' ) ) )
 			$redirect = apply_filters( 'flp/redirect_url', '');
 
-		echo apply_filters('fbl/login_button', '<a href="#" class="css-fbl js-fbl" data-redirect="'.$redirect.'" data-fb_nonce="' . wp_create_nonce( 'facebook-nonce' ).'"><div>'. __('Connect with Facebook', $this->plugin_name) .'<img src="'.site_url('/wp-includes/js/mediaelement/loading.gif').'" alt=""/></div></a>');
+		echo apply_filters('fbl/login_button', '<a href="#" class="css-fbl js-fbl" data-redirect="'.$redirect.'" data-fb_nonce="' . wp_create_nonce( 'facebook-nonce' ).'"><div>'. __('Connect with Facebook', $this->plugin_name) .'<img src="'.site_url('/wp-includes/js/mediaelement/loading.gif').'" alt="" style="display:none"/></div></a>');
 	}
 
 	/**
